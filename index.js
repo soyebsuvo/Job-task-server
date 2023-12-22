0721b4776daaeef99a6e0c177e303bd669d42dc7
soyebsuvo <soyebbeen@gmail.com>
2023-12-22 15:34:03 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qnjuxu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const database = client.db('TodoDB');
+    const todoCollections = database.collection('todos');
+
+    app.post('/alltodos' , async (req , res) => {
+        const newTodo = req.body;
+        console.log(newTodo)
+        const result = await todoCollections.insertOne(newTodo);
+        console.log(result)
+        res.send(result);
+    })
+
+    app.get("/todos" , async (req , res) => {
+        const result = await todoCollections.find().toArray();
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/" , (req , res) => {
+    res.send('Task Management Server is running.....')
+})
+
+app.listen(port , () => {
+    console.log(`Server is running on port : ${port}`);
+})
